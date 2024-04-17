74358614ff7428ab88474dc1b15cefd517349e36
EWBr0wn <31904126+EWBr0wn@users.noreply.github.com>
2024-04-17 07:26:49 -0400

    Update entrypoint.sh
    More exploration of what can be done inside the container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,19 @@
 if [ -f /etc/os-release ] ; then
   . /etc/os-release
 fi
+
+echo "# List env vars"
+env | sort
+echo
+
+echo "# List recursively /usr/src"
+ls -lhR /usr/src
+echo
+
+echo "# List GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
+ls -lhR ${GITHUB_WORKSPACE}
+echo
+
 # Use INPUT_<INPUT_NAME> to get the value of an input
 GREETING="Hello, $INPUT_WHO_TO_GREET! from $PRETTY_NAME"
 
