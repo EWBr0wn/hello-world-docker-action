7bf81d4cc0b74cef1b490b1ec3464ac2f28cd2b7
EWBr0wn <31904126+EWBr0wn@users.noreply.github.com>
2024-04-20 17:25:07 -0400

    Update entrypoint.sh
    rsync not there so replaced with cp and options

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,15 +16,15 @@ echo "# List GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}" && \
   ls -lhR ${GITHUB_WORKSPACE} && \
   echo
 
-REPO_SPEC_DIR=$(dirname ${INPUT_SPEC_FILE}) && \
-  REPO_SPEC_FILENAME=$(basename ${INPUT_SPEC_FILE}) && \
-  rsync -av ${GITHUB_WORKSPACE}/${INPUT_SPEC_FILE} /usr/src/rpmbuild/SPECS/
-
-echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) patches: ${INPUT_SPEC_FILE}" && \
-  spectool --sources /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
-
-echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) sources: ${INPUT_SPEC_FILE}" && \
-  spectool --patches /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+if [ -n "${INPUT_SPEC_FILE}" ] ; then
+  REPO_SPEC_DIR=$(dirname ${INPUT_SPEC_FILE}) && \
+    REPO_SPEC_FILENAME=$(basename ${INPUT_SPEC_FILE}) && \
+    cp --archive --verbose ${GITHUB_WORKSPACE}/${INPUT_SPEC_FILE} /usr/src/rpmbuild/SPECS/
+  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) patches: ${INPUT_SPEC_FILE}" && \
+    spectool --sources /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) sources: ${INPUT_SPEC_FILE}" && \
+    spectool --patches /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+fi
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
 GREETING="Hello, $INPUT_WHO_TO_GREET! from $PRETTY_NAME"
