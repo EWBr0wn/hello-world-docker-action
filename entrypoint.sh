6b819e8063b8adc726b98c15dc74bbe72ae69ad2
EWBr0wn <31904126+EWBr0wn@users.noreply.github.com>
2024-04-20 17:17:34 -0400

    Update entrypoint.sh
    Start taking input and ensuring bits are available for rpmbuild.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,27 @@ if [ -f /etc/os-release ] ; then
   . /etc/os-release
 fi
 
-echo "# List env vars"
-env | sort
-echo
+echo "# List env vars" && \
+  env | sort && \
+  echo
 
-echo "# List recursively /usr/src"
-ls -lhR /usr/src
-echo
+echo "# List recursively /usr/src" && \
+  ls -lhR /usr/src && \
+  echo
 
-echo "# List GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
-ls -lhR ${GITHUB_WORKSPACE}
-echo
+echo "# List GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}" && \
+  ls -lhR ${GITHUB_WORKSPACE} && \
+  echo
+
+REPO_SPEC_DIR=$(dirname ${INPUT_SPEC_FILE}) && \
+  REPO_SPEC_FILENAME=$(basename ${INPUT_SPEC_FILE}) && \
+  rsync -av ${GITHUB_WORKSPACE}/${INPUT_SPEC_FILE} /usr/src/rpmbuild/SPECS/
+
+echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) patches: ${INPUT_SPEC_FILE}" && \
+  spectool --sources /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+
+echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) sources: ${INPUT_SPEC_FILE}" && \
+  spectool --patches /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
 GREETING="Hello, $INPUT_WHO_TO_GREET! from $PRETTY_NAME"
