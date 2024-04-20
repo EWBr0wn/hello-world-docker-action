0fa2ac000f22bcf5253d58bc00c35ed4f943fb8b
EWBr0wn <31904126+EWBr0wn@users.noreply.github.com>
2024-04-20 17:42:04 -0400

    Update entrypoint.sh
    make JSON from spec file SOURCE and PATCH lists

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,14 +16,21 @@ echo "# List GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}" && \
   ls -lhR ${GITHUB_WORKSPACE} && \
   echo
 
+echo "INPUT_SPEC_FILE: ${INPUT_SPEC_FILE}"
 if [ -n "${INPUT_SPEC_FILE}" ] ; then
-  REPO_SPEC_DIR=$(dirname ${INPUT_SPEC_FILE}) && \
-    REPO_SPEC_FILENAME=$(basename ${INPUT_SPEC_FILE}) && \
-    cp --archive --verbose ${GITHUB_WORKSPACE}/${INPUT_SPEC_FILE} /usr/src/rpmbuild/SPECS/
-  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) patches: ${INPUT_SPEC_FILE}" && \
-    spectool --sources /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
-  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) sources: ${INPUT_SPEC_FILE}" && \
-    spectool --patches /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+  REPO_SPEC_DIR=$(dirname ${INPUT_SPEC_FILE})
+  REPO_SPEC_FILENAME=$(basename ${INPUT_SPEC_FILE})
+  rsync --archive --verbose ${GITHUB_WORKSPACE}/${INPUT_SPEC_FILE} /usr/src/rpmbuild/SPECS/
+
+  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) patches: ${INPUT_SPEC_FILE}"
+  spectool --sources /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+  SOURCES=$(spectool --sources /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME} | yq -o=json . )
+  echo ${SOURCES} | jq .
+
+  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) sources: ${INPUT_SPEC_FILE}"
+  spectool --patches /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME}
+  PATCHES=$(spectool --patches /usr/src/rpmbuild/SPECS/${REPO_SPEC_FILENAME} | yq -o=json . )
+  echo ${PATCHES} | jq .
 fi
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
