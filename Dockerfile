3faee98837306c84bb74740e28f6fd904ac99160
EWBr0wn <31904126+EWBr0wn@users.noreply.github.com>
2024-05-25 22:50:49 -0400

    Rockylinux dev into Rockylinux (#1)
    * Add rockylinux-dev branch in workflow.
    
    * Setup build tree and scripts for RPMs in /usr/src/rpmbuild.
    
    * Update action.yml
    
    Add inputs and outputs
    
    Adding inputs and the artifact_array output
    
    fix who_to_greet by moving to snake_case
    
    * Update description for additional_repos and add repo when provided
    See document at https://github.com/marketplace/actions/rpm-build for additional_repos where it shows the setting looks like the following:
    
    "['centos-release-scl', 'http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm']"
    
    * Update Dockerfile to add rsync and jq
    
    * Update entrypoint.sh
    
    Start taking input and ensuring bits are available for rpmbuild.
    
    make JSON from spec file SOURCE and PATCH lists
    
    download URLs, position sources and patches
    
    Not sure what is incorrect for input spec file - skipping exit if rpmlint returns non-zero
    
    Add yum-utils and yum-config-manager
    
    Add retval for rpmbuild
    
    SRPM not showing src - bug? in rpmspec maybe
    
    build option for rsync to copy source files that were downloaded
    
    Work to build out artifact JSON and write file to output directory
    
    $GITHUB_OUTPUT is quoted in the docs
    
    * Fix yum-builddep install
    
    * Where did .rpmmacros go from docker
    
    code added to recreate in current user context

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,20 @@
 # Set the base image to use for subsequent instructions
 FROM rockylinux:8.9
 
+# Install elements for building RPMs
+RUN yum install --setopt=tsflags=nodocs -y \
+      rpm-build \
+      rpmdevtools \
+      rpmlint \
+      redhat-rpm-config \
+      rsync \
+      yum-utils \
+      jq \
+    && yum clean all \
+    && rm -rf /var/cache/yum \
+    && echo '%_topdir /usr/src/rpmbuild' > ~/.rpmmacros \
+    && rpmdev-setuptree
+
 # Set the working directory inside the container
 WORKDIR /usr/src
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,280 @@
 #!/bin/sh -l
-
+#set -x
 if [ -f /etc/os-release ] ; then
   . /etc/os-release
+  echo "::notice file=entrypoint.sh,line=5::${PRETTY_NAME}"
 fi
-pwd
+
+if [ -n "${PRETTY_NAME}" ] ; then
+  echo "::group::List env vars"
+  env | sort
+  echo "::endgroup::"
+fi
+
+# Parameterize Verbose, debug_package, and _debugsource_template
+#INPUT_SPEC_FILE=contrib/**/*.spec
+if [ "${INPUT_VERBOSE}" = 'false' ] || [ -z "${INPUT_VERBOSE}" ] ; then
+  # default 'INPUT_VERBOSE=false'
+  LONG_VERBOSE=''
+  SHORT_VERBOSE=''
+else
+  LONG_VERBOSE='--verbose'
+  SHORT_VERBOSE='-v'
+fi
+## Check if input variable set for more detailed JSON return set
+if [ "${INPUT_OUTPUT_DL_ARTIFACTS}" = 'false' ] || [ -z "${INPUT_OUTPUT_DL_ARTIFACTS}" ] ; then
+  # default INPUT_OUTPUT_DL_ARTIFACTS=false
+  ## description: 'Add any downloaded artifacts to artifact_array'
+  INCLUDEDOWNLOADS='false'
+else
+  INCLUDEDOWNLOADS='true'
+fi
+output_array='[]'
+if [ "${INPUT_DEBUG_RPMS}" = 'true' ] || [ -z "${INPUT_DEBUG_RPMS}" ] ; then
+  # default INPUT_DEBUG_RPMS=true
+  LONG_RPM_DEBUG_PACKAGE=''
+else
+  LONG_RPM_DEBUG_PACKAGE='--nodebuginfo'
+fi
+
+echo "::group::user home directory ~/.rpmmacros"
+if [ ! -f ~/.rpmmacros ] ; then
+  echo '%_topdir /usr/src/rpmbuild' > ~/.rpmmacros
+fi
+cat ~/.rpmmacros
+echo "::endgroup::"
+
+echo "INPUT_SPEC_FILE: ${INPUT_SPEC_FILE}"
+if [ -z "${INPUT_SPEC_FILE}" ] ; then
+  # no input provided, so set to default
+  ## cd to the base of the git checkout, then search for files matching '*.spec'
+  ##   output into JSON array to make things easier
+  INPUT_SPEC_FILE=$(cd ${GITHUB_WORKSPACE} ; find . -name "*.spec" | head -1 | sed 's#./##')
+fi
+
+RPMBUILDSPECSDIR=$(rpm --eval "%_specdir")
+RPMBUILDSOURCEDIR=$(rpm --eval "%_sourcedir")
+# if the INPUT_SPEC_FILE variable is empty, then 
+#   search the checked out directory structure to find *.spec
+if [ -n "${INPUT_SPEC_FILE}" ] ; then
+  REPO_SPEC_DIR=$(dirname ${INPUT_SPEC_FILE})
+  REPO_SPEC_FILENAME=$(basename ${INPUT_SPEC_FILE})
+
+  cp --archive ${LONG_VERBOSE} ${GITHUB_WORKSPACE}/${INPUT_SPEC_FILE} ${RPMBUILDSPECSDIR}/
+
+  if [ -n "${ADDITIONAL_REPOS}" ] ; then
+    echo "${ADDITIONAL_REPOS}" | jq -r .[]
+    for repo in $(echo "${ADDITIONAL_REPOS}" | jq -r .[]) ; do
+      yum-config-manager --enable ${repo}
+      ## Correct answer is to test for URL or string
+      # yum install -y ${repo}
+    done
+  fi
+
+  echo "# rpmlint the SPEC_FILE: ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}"
+  rpmlint ${LONG_VERBOSE} ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+  retval=$?
+  if [ ${retval} -gt 0 ] ; then
+    echo "## retval=${retval} from rpmlint"
+  #  exit ${retval}
+  fi
+
+  ## Check to see if input variable set for more detailed JSON return set
+  
+  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) sources: ${INPUT_SPEC_FILE}"
+  spectool --sources ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+  # Make list of files that will be copied
+  for f in $(spectool --sources ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | egrep -v 'http[s]*://|ftp://' | awk '{print $2}') ; do
+    cp --archive ${LONG_VERBOSE} ${GITHUB_WORKSPACE}/${REPO_SPEC_DIR}/../SOURCES/${f} ${RPMBUILDSOURCEDIR}/
+  done
+
+  echo "# List SPEC_FILE (${REPO_SPEC_FILENAME}) patches: ${INPUT_SPEC_FILE}"
+  spectool --patches ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+  # Make list of patches that will be copied
+  for p in $(spectool --patches ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | egrep -v 'http[s]*://|ftp://' | awk '{print $2}') ; do
+    cp --archive ${LONG_VERBOSE} ${GITHUB_WORKSPACE}/${REPO_SPEC_DIR}/../SOURCES/${p} ${RPMBUILDSOURCEDIR}/
+  done
+
+  echo "# Fetch Source and Patches files from URLs"
+  SRCLISTBEFORE="$(ls -1 ${RPMBUILDSOURCEDIR} | jq -R '[.]' | jq -s -c 'add')"
+  spectool --get-files --sourcedir ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+  if [ "${INCLUDEDOWNLOADS}" = 'true' ] ; then
+    SRCLISTAFTER="$(ls -1 ${RPMBUILDSOURCEDIR} | jq -R '[.]' | jq -s -c 'add')"
+  else
+    SRCLISTAFTER="${SRCLISTBEFORE}"
+  fi
+  # take every file from SRCLISTBEFORE out of SRCLISTAFTER
+  DLSRCLIST="$(jq -n -c --argjson fbefore "${SRCLISTBEFORE}" --argjson fafter "${SRCLISTAFTER}" '$fafter - $fbefore')"
+  # make temp file
+  SRCEXCLUDETMPFILE=$(mktemp -q /tmp/.source-rsync-exclude.XXXXXX)
+  jq -n -c --argjson fbefore "${SRCLISTBEFORE}" --argjson fafter "${SRCLISTAFTER}" '$fbefore - ($fafter - $fbefore)' | \
+    jq -r '.[]' | \
+    sed 's#^#SOURCES/#' | tee ${SRCEXCLUDETMPFILE}
+  if [ "${INCLUDEDOWNLOADS}" = 'true' ] ; then
+    RSYNC_DL_SRC_ADDON="--exclude-from=${SRCEXCLUDETMPFILE} ${RPMBUILDSOURCEDIR}"
+  else
+    RSYNC_DL_SRC_ADDON=''
+  fi
+
+  echo "## List files in SOURCES directory"
+  ls -l ${RPMBUILDSOURCEDIR}
+
+  echo "# Using yum-builddep from yum-utils to install all the build dependencies for a package"
+  yum-builddep -y ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+
+  # consider input parameters that control '-v', '-vv', and '--define "debug_package %{nil}"' independently
+  echo "# Build RPM"
+  rpmbuild -ba ${LONG_RPM_DS_T} ${LONG_RPM_DEBUG_PACKAGE} ${SHORT_VERBOSE} ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+  retval=$?
+  if [ ${retval} -gt 0 ] ; then
+    echo "## retval=${retval} from rpmbuild"
+  #  exit ${retval}
+  fi
+
+  # Command to get Name-Version-Release string from SPEC file
+  ## $ rpmspec --define "dist %{nil}" ${LONG_RPM_DEBUG_PACKAGE} ${LONG_RPM_DS_T} --query --queryformat="%{nvr}" ${SPEC}
+  nvr=$(rpmspec --define "dist %{nil}" --query --srpm --queryformat="%{nvr}" ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME})
+  dist=$(rpm --eval "%dist" | sed 's#\.##')
+
+  echo "# List output RPMs"
+  ls -lR $(rpm --eval "%_rpmdir")
+  echo "# List expected RPMs"
+  rpmspec --query --rpms ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | jq -R -s -c 'split("\n") | map(select(length>0))'
+  tmp_rpm_array=$(rpmspec --query --rpms ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | jq -R -s -c 'split("\n") | map(select(length>0))')
+
+  echo "# List output SRPM"
+  ls -lR $(rpm --eval "%_srcrpmdir")
+  echo "## debug rpmspec"
+  tmpstring=$(rpmspec --query --srpm --queryformat="%{arch}" ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME})
+  echo "##               - ${tmpstring}"
+  echo "# List expected SRPM"
+  if [ "${tmpstring}" = "src" ] ; then
+    rpmspec --query --srpm ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | jq -R -s -c 'split("\n") | map(select(length>0))'
+    tmp_srpm_array="$(rpmspec --query --srpm ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | jq -R -s -c 'split("\n") | map(select(length>0))')"
+  else
+    ## Potential bug in rpmspec where the Source RPM does not have arch=src
+    echo "::notice file=entrypoint.sh,line=156::Mitigating rpmspec bug"
+    rpmspec --query --srpm --queryformat="%{name}-%{version}-%{release}.src\n" ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME}
+    tmp_srpm_array="$(rpmspec --query --srpm --queryformat="%{name}-%{version}-%{release}.src" ${RPMBUILDSPECSDIR}/${REPO_SPEC_FILENAME} | jq -R -s -c 'split("\n") | map(select(length>0))')"
+    ##
+  fi
+
+  # If all is good so far, create output directory
+  if [ ! -d ${GITHUB_WORKSPACE}/output ] ; then
+    mkdir ${LONG_VERBOSE} ${GITHUB_WORKSPACE}/output
+    chown ${LONG_VERBOSE} --reference=${GITHUB_WORKSPACE} ${GITHUB_WORKSPACE}/output
+  fi
+
+  # Copy output RPMs and if external variable set, copy all files in %_sourcedir as well
+  rsync --archive ${LONG_VERBOSE} ${RSYNC_DL_SRC_ADDON} $(rpm --eval "%_rpmdir") $(rpm --eval "%_srcrpmdir") ${GITHUB_WORKSPACE}/output/
+  chown ${LONG_VERBOSE} --recursive --reference=${GITHUB_WORKSPACE} ${GITHUB_WORKSPACE}/output/
+  # make temp file
+  TMPFILE=$(mktemp -q /tmp/.filearray-egrep.XXXXXX)
+  echo ${tmp_rpm_array} | jq -r .[] | awk '{print "/" $1}' | tee ${TMPFILE}
+  echo "# finding files"
+  find output -type f
+  echo "## rpm  - find files matching the following"
+  cat ${TMPFILE}
+  echo "# make rpm_array"
+  find output -type f | egrep -f ${TMPFILE} | jq -R -s -c 'split("\n") | map(select(length>0))'
+  rpm_array=$(find output -type f | egrep -f ${TMPFILE} | jq -R -s -c 'split("\n") | map(select(length>0))')
+  echo ${tmp_srpm_array} | jq -r .[] | awk '{print "/" $1}' | tee ${TMPFILE}
+  echo "## srpm - find files matching the following"
+  cat ${TMPFILE}
+  echo "# make srpm_array"
+  find output -type f | egrep -f ${TMPFILE} | jq -R -s -c 'split("\n") | map(select(length>0))'
+  srpm_array=$(find output -type f | egrep -f ${TMPFILE} | jq -R -s -c 'split("\n") | map(select(length>0))')
+  echo "# make src_array"
+  echo ${DLSRCLIST} | jq -r .[] | awk '{print "/" $1}' | tee ${TMPFILE}
+  find output/SOURCES -type f | egrep -f ${TMPFILE} | jq -R -s -c 'split("\n") | map(select(length>0))'
+  src_array=$(find output/SOURCES -type f | egrep -f ${TMPFILE} | jq -R -s -c 'split("\n") | map(select(length>0))')
+
+  # set outputs
+  # built_rpm_array:
+  #  description: 'JSON array of built RPM files'
+  echo "built_rpm_array=${rpm_array}" >> "$GITHUB_OUTPUT"
+  # built_srpm_array:
+  #  description: 'JSON array of SRPM'
+  echo "built_srpm_array=${srpm_array}" >> "$GITHUB_OUTPUT"
+  
+  # description: 'Content-type for Upload'
+  echo "rpm_content_type=application/x-rpm" >> "$GITHUB_OUTPUT"
+
+  # build out the output_array and include ${DLSRCLIST} if set
+  ## for each file in ${DLSRCLIST} construct JSON of filename, fullpath, size, MD5, SHA256, modification time, etc.
+  JSONFILELIST=$(mktemp -q /tmp/.Artifacts-list.XXXXXX)
+  # There appears to be a size limit different from the +1M limit for the return to GitHub actions
+  ## Creating a file in the "output" directory for other steps to have access to the full JSON built
+  JSONOUTPUTFILELIST="${GITHUB_WORKSPACE}/output/full_artifact_array.${dist}.json"
+  for f in $(echo ${rpm_array} | jq -r '.[]') ; do
+    #rpm -qpi ${f}
+    tj=$(stat --printf='{"FullPath":"%n","Size":%s,"ModifiedSSE":%Y}\n' ${f} | jq -c .)
+    sse=$(echo ${tj} | jq '.ModifiedSSE')
+    echo ${tj} | jq \
+                    --arg n "$(basename ${f})" \
+                    --arg d "$(date --date="@${sse}" --iso-8601=seconds | tr -d ':-' | sed 's#+0000$#Z#')" \
+                    --arg m "$(md5sum ${f} | awk '{print $1}')" \
+                    --arg s "$(sha256sum ${f} | awk '{print $1}')" \
+                    '. + {"Name": $n, "Modified": $d, "MD5": $m, "SHA256": $s}' | \
+      jq -c --arg t "$(rpm -q --queryformat="%{arch}" ${f})" '{"Name", "FullPath", "Size", "Modified", "ModifiedSSE", "MD5", "SHA256", "Type": $t}' | \
+      tee -a ${JSONOUTPUTFILELIST} | \
+      jq -c '{"Name", "FullPath", "ModifiedSSE", "Type"}' | \
+      tee -a ${JSONFILELIST}
+  done
+  for f in $(echo ${srpm_array} | jq -r '.[]') ; do
+    rpm -qpi ${f}
+    tj=$(stat --printf='{"FullPath":"%n","Size":%s,"ModifiedSSE":%Y}\n' ${f} | jq -c .)
+    sse=$(echo ${tj} | jq '.ModifiedSSE')
+    echo ${tj} | jq \
+                    --arg n "$(basename ${f})" \
+                    --arg d "$(date --date="@${sse}" --iso-8601=seconds | tr -d ':-' | sed 's#+0000$#Z#')" \
+                    --arg m "$(md5sum ${f} | awk '{print $1}')" \
+                    --arg s "$(sha256sum ${f} | awk '{print $1}')" \
+                    '. + {"Name": $n, "Modified": $d, "MD5": $m, "SHA256": $s}' | \
+      jq -c --arg t "srpm" '{"Name", "FullPath", "Size", "Modified", "ModifiedSSE", "MD5", "SHA256", "Type": $t}' | \
+      tee -a ${JSONOUTPUTFILELIST} | \
+      jq -c '{"Name", "FullPath", "ModifiedSSE", "Type"}' | \
+      tee -a ${JSONFILELIST}
+  done
+  if [ "${INCLUDEDOWNLOADS}" = "true" ] ; then
+    for f in $(echo ${src_array} | jq -r '.[]') ; do
+      tj=$(stat --printf='{"FullPath":"%n","Size":%s,"ModifiedSSE":%Y}\n' ${f} | jq -c .)
+      sse=$(echo ${tj} | jq '.ModifiedSSE')
+      echo ${tj} | jq \
+                      --arg n "$(basename ${f})" \
+                      --arg d "$(date --date="@${sse}" --iso-8601=seconds | tr -d ':-' | sed 's#+0000$#Z#')" \
+                      --arg m "$(md5sum ${f} | awk '{print $1}')" \
+                      --arg s "$(sha256sum ${f} | awk '{print $1}')" \
+                      '. + {"Name": $n, "Modified": $d, "MD5": $m, "SHA256": $s}' | \
+        jq -c --arg t "dlsrc" '{"Name", "FullPath", "Size", "Modified", "ModifiedSSE", "MD5", "SHA256", "Type": $t}' | \
+        tee -a ${JSONOUTPUTFILELIST} | \
+        jq -c '{"Name", "FullPath", "ModifiedSSE", "Type"}' | \
+        tee -a ${JSONFILELIST}
+    done
+  fi
+  echo "::group::List JSON file list"
+  ls -lh ${JSONFILELIST}
+  cat ${JSONFILELIST} | nl
+  echo "::endgroup::"
+  # put some test here in the future when size limit is better known
+  JSONOUTPUTARRAY=$(mktemp -q /tmp/.Artifact-array.XXXXXX)
+  #cat ${JSONFILELIST} | jq -c --slurp --arg nvr "${nvr}" --arg dist "${dist}" '[{"NVR":$nvr,"artifactsets":[{"Dist":$dist,"Artifacts": . }]}]' | tee ${JSONOUTPUTARRAY}
+  #output_array=$(cat ${JSONFILELIST} | jq -c --slurp --arg nvr "${nvr}" --arg dist "${dist}" '[{"NVR":$nvr,"artifactsets":[{"Dist":$dist,"Artifacts": . }]}]')
+  cat ${JSONOUTPUTFILELIST} | jq -c --slurp --arg nvr "${nvr}" --arg dist "${dist}" '[{"NVR":$nvr,"artifactsets":[{"Dist":$dist,"Artifacts": . }]}]' | tee ${JSONOUTPUTARRAY}
+  # insert test here to see if output array is too large; mitigate if too large
+  echo "artifact_array=$(cat ${JSONOUTPUTARRAY})" >> "$GITHUB_OUTPUT"
+fi  # end of if from line 64
+
 # Use INPUT_<INPUT_NAME> to get the value of an input
 GREETING="Hello, $INPUT_WHO_TO_GREET! from $PRETTY_NAME"
 
 # Use workflow commands to do things like set debug messages
-echo "::notice file=entrypoint.sh,line=7::$GREETING"
+echo "::notice file=entrypoint.sh,line=272::$GREETING"
 
-# Write outputs to the $GITHUB_OUTPUT file
-echo "greeting=$GREETING" >>"$GITHUB_OUTPUT"
+# Write outputs to the "$GITHUB_OUTPUT" file
+## echo "greeting=$GREETING" >> "$GITHUB_OUTPUT"
+## Using tee gleaned from the following:
+## https://github.com/FirelightFlagboy/action-gh-release-test/blob/da8751c8d19233021a65a59c448ca26d344b5a89/.github/workflows/release-test.yml#L30
+echo "greeting=$GREETING" | tee -a "$GITHUB_OUTPUT"
 
 exit 0
